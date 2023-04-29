9d65c8110db9ed4084e4de2aad164e046ccc2483
Asa Choi <asa.choi@gmail.com>
2023-04-29 04:34:55 +0000

    Update 13 files

--- index.js
+++ index.js
@@ -29,6 +29,86 @@ async function getTranscription(file) {
   return result.data.text;
 }
 
+async function getSummary(text) {
+  var axios = require("axios");
+  const systemMsg = `You are an assistant that only speaks in Markdown. Do not write text that isn't formatted as markdown.
+
+Example formatting:
+
+Testing No-Code Workflow
+
+--Summary--
+
+This audio recording documents a test of a no-code workflow using Google Drive and a single code step to reduce calls and improve efficiency.
+
+--Additional Info--
+
+## Main Points
+
+- point 1
+- point 2
+
+## Action Items
+
+- point 1
+- point 2
+
+## Follow Up Questions
+
+- point 1
+- point 2
+
+## Potential Arguments Against
+
+- point 1
+- point 2
+  `;
+  const userMsg = `Write a Title for the transcript that is under 15 words.
+
+Then write: "--Summary--"
+
+Write "Summary" as a Heading 1.
+
+Write a summary of the provided transcript.
+
+Then write: "--Additional Info--".
+
+Then return a list of the main points in the provided transcript. Then return a list of action items. Then return a list of follow up questions. Then return a list of potential arguments against the transcript.
+
+For each list, return a Heading 2 before writing the list items. Limit each list item to 100 words, and return no more than 5 points per list.
+
+Transcript:
+
+${text}`;
+
+  messages = [
+    { role: "system", content: systemMsg },
+    { role: "user", content: userMsg },
+  ];
+
+  var data = JSON.stringify({
+    model: "gpt-3.5-turbo",
+    messages,
+    temperature: 0.8,
+  });
+
+  var config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "https://api.openai.com/v1/chat/completions",
+    headers: {
+      Authorization: `Bearer ${process.env.apiKey}`,
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  const result = await axios(config);
+
+  //console.log({ result: result.data.choices[0].message });
+  return result.data.choices[0].message;
+}
+
 app.post("/upload", upload.single("file"), async (req, res, next) => {
   const file = req.file;
   if (!file) {
@@ -38,8 +118,9 @@ app.post("/upload", upload.single("file"), async (req, res, next) => {
   }
 
   const result = await getTranscription(file.path);
+  const summary = await getSummary(result);
 
-  res.json({ result });
+  res.json({ result, summary });
 });
 
 app.get("/", async (req, res) => {
